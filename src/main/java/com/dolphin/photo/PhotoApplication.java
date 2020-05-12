dbf8097ac8ca903a96919834571ab78c4af125e3
whb <wanghaibing@shein.com>
2020-05-12 13:55:47 +0800

    some

--- src/main/java/com/dolphin/photo/PhotoApplication.java
+++ src/main/java/com/dolphin/photo/PhotoApplication.java
@@ -2,7 +2,13 @@ package com.dolphin.photo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * @author whb
+ */
 @SpringBootApplication
 public class PhotoApplication {
 
@@ -10,4 +16,17 @@ public class PhotoApplication {
 		SpringApplication.run(PhotoApplication.class, args);
 	}
 
+	/**
+	 * Global CORS configuration
+	 * @return WebMvcConfigurer
+	 */
+	@Bean
+	public WebMvcConfigurer corsConfigurer () {
+		return new WebMvcConfigurer () {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST");
+			}
+		};
+	}
 }
